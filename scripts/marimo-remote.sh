1096d66e2f1e078ca03dc37a98c1d3a8c52ef5ef
panikk <buttfacecat2211@gmail.com>
2025-06-03 01:27:51 +0700

    I spent a whole night debugging a ssh port forwarding script

--- scripts/marimo-remote.sh
+++ scripts/marimo-remote.sh
@@ -1,22 +1,20 @@
-#!/user/bin/env bash
+#!/usr/bin/env bash
+
+set -xe
 
 if [ $# -eq 0 ]; then
-		echo "Usage: $0 user@hostname [port] [remote_directory]"
-		echo "Example: $0 john@server.com"
-		echo "Example: $0 john@server.com 8080 ~/my-project"
-		exit 1
+    echo "Usage: $0 user@hostname [port] [remote_directory]"
+    echo "Example: $0 john@server.com"
+    echo "Example: $0 john@server.com 8080 ~/my-project"
+    exit 1
 fi
 
 USER_HOST="$1"
-PORT="${2:-2718}" 
-REMOTE_DIR="${3:-$(pwd | sed 's|.*/||')}"  
+SSH_PORT="$2"
+PORT="${3:-2718}"
 
-echo "Starting marimo on $USER_HOST"
-echo "Remote directory: $REMOTE_DIR"
-echo "Local access: http://localhost:$PORT"
-echo "Press Ctrl+C to stop"
-echo ""
-
-# SSH with port forwarding and run marimo
-ssh -L "$PORT:localhost:$PORT" "$USER_HOST" \
-    "cd $REMOTE_DIR && uv run marimo edit --watch --host 0.0.0.0 --port $PORT"
+ssh -N -i ~/.ssh/runpod \
+    -p "$SSH_PORT" \
+    -L "0.0.0.0:$PORT:127.0.0.1:$PORT" \
+    -o "ExitOnForwardFailure=yes" \
+    "$USER_HOST" -vvv
